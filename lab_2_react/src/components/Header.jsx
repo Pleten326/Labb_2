8bb56faac406ace73916d64237982e165f693e0e
Pavlo <pletenytskyy31@gmail.com>
2026-04-03 11:04:08 +0300

    update design

--- lab_2_react/src/components/Header.jsx
+++ lab_2_react/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <header className="flex flex-col items-center bg-slate-900 text-white p-10 rounded-3xl shadow-2xl border-b-4 border-sky-500 mb-8">
-      <h1 className="text-5xl font-black tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-emerald-400 mb-2">
+      <h1 className="text-5xl font-black tracking-tighter text-transparent bg-clip-text bg-gradient-to-r from-green-400 to-emerald-400 mb-2">
         ПАВЛО
       </h1>
       <p className="text-lg font-light tracking-widest uppercase text-slate-400 mb-6">
